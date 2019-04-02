244c2bf8988ab983df48a4fc38c86fef70ba0255
TUHAMA <tuhama9@yahoo.com>
2019-04-02 15:48:34 +0300

    initial

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -1,19 +1,35 @@
 import React, {Component} from 'react';
 import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
 
+
+const otherFontTheme = {
+    background: '#f5f8fb',
+    fontFamily: 'Helvetica Neue',
+    headerBgColor: 'rgb(49, 181, 172)',
+    headerFontColor: '#fff',
+    headerFontSize: '16px',
+    botBubbleColor: 'rgb(49, 181, 172)',
+    botFontColor: '#fff',
+    userBubbleColor: '#fff',
+    userFontColor: '#4a4a4a'
+};
 
 export default class UniChatBot extends Component {
+
     render() {
         return (
             <div className=''>
-                <ChatBot
+                <ThemeProvider theme={otherFontTheme}>
+                <ChatBot floating={true} opened={false}
                     steps={[
                         {
-                            id: 'hello-world',
-                            message: 'Hello World!',
+                            id: 'hello',
+                            message: 'Hello!',
                             end: true,
                         },
                     ]}
                 />
+                </ThemeProvider>
             </div>)}
 }
